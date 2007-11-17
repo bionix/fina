8904903b1d177fe11f03002bb17eaf78b87483a9
Tobias Klausmann <klausman@schwarzvogel.de>
2007-11-17 20:51:05 +0000

    Changed all instances of [] to [[]] - less quoting and no exec()s
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@16 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- fina.sh
+++ fina.sh
@@ -19,7 +19,7 @@ IPTSAVE=$(which iptables-save)
 
 PROGNAME="$0"
 VERSION="0.1"
-DEBUG="0"
+DEBUG="1"
 
 # Help function
 function print_help() {
@@ -34,7 +34,7 @@ function print_help() {
 
 # Debug printing 
 function dprint() {
-    if [ 1 == "${DEBUG}" ]; then
+    if [[ $DEBUG == 1 ]]; then
         echo  -e "$@" >&2
     fi
 }
@@ -57,13 +57,13 @@ function get_tmpdir () {
     local TRIES=3
     local TMPDIR=''
     umask 0077
-    while [ "$OK" != 0 -a "$TRIES" -gt 0 ]; do
+    while [[ $OK != 0 && "$TRIES" -gt 0 ]]; do
         TMPDIR=/tmp/fina-$RANDOM$RANDOM
         TRIES=$(($TRIES-1))
         dprint "Trying to create $TMPDIR (tries left: $TRIES)" >&2
         mkdir "$TMPDIR" && OK=0
     done
-    if [ $OK != 0 ]; then
+    if [[ $OK != 0 ]]; then
         echo "Could not create TMPDIR."
         exit -1
     fi
@@ -116,7 +116,7 @@ dprint "Found:\n$RULEFILES"
 TMPDIR=$(get_tmpdir)
 
 # Save the old rules if we're not pretending
-if [ "$PRETEND" != 1 ]; then
+if [[ $PRETEND != 1 ]]; then
     $IPTSAVE > $TMPDIR/rules.old
 fi
 
@@ -135,19 +135,19 @@ fi
     date -Is 
 } > "$TMPDIR/rules.new"
 
-if [ "$PRETEND" == 1 ]; then
+if [[ $PRETEND == 1 ]]; then
     cat "$TMPDIR/rules.new"
 else
     dprint "Loading rules from file $TMPDIR/rules.new"
     MESG=$(iptables-restore < "$TMPDIR/rules.new" 2>&1)
     RETVAL=$?
-    if [ "$RETVAL" != "0" ]; then
+    if [[ $RETVAL != 0 ]]; then
         eprint "Something went wrong (retval $RETVAL). Message:"
         eprint "$MESG"
         eprint "Loading old ruleset from $TMPDIR/rules.old"
         MESG=$(iptables-restore < "$TMPDIR/rules.old" 2>&1)
         RETVAL=$?
-        if [ "$RETVAL" != "0" ]; then
+        if [[ $RETVAL != 0 ]]; then
             eprint "Even the old ruleset could not be loaded. Message:"
             eprint "$MESG"
         fi
@@ -156,7 +156,7 @@ else
     fi
 fi
 
-if [ "$KEEPTEMP" != 1 ]; then
+if [[ $KEEPTEMP != 1 ]]; then
     rm -rf $TMPDIR
 fi
 
