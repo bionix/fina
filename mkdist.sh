cea5235db9f88ffc3c10405715efa9cc44eaedf7
Tobias Klausmann <klausman@schwarzvogel.de>
2008-01-22 19:49:04 +0000

    Post/pre-Skript are now part of thr repository
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@53 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- mkdist.sh
+++ mkdist.sh
@@ -10,7 +10,7 @@ FVERS=$(./fina.sh --version|head -n1|sed -e 's/.*version //g') || die "Could not
 PNAME="fina-$FVERS"
 
 mkdir "$PNAME" || die "mkdir failed"
-cp -r fina.sh example-rules.d fina.cfg INSTALL COPYING README fina.init fina.init.gentoo "fina-$FVERS/" || die "cp failed"
+cp -r fina.sh example-rules.d fina.cfg INSTALL COPYING README fina.init fina.init.gentoo post-up.sh pre-up.sh "fina-$FVERS/" || die "cp failed"
 find "$PNAME" -name '.svn' -print0 |xargs -0 rm -r || die "find failed"
 tar czf "$PNAME".tar.gz "$PNAME" || die "tar failed"
 rm -rf "$PNAME" || die "rm failed"
