11acecf75e6b4ca2f2a0d4e576a2760e9f9affd3
Tobias Klausmann <klausman@schwarzvogel.de>
2008-02-27 13:05:42 +0000

    Don't use exit as we're being sourced, not run.
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@56 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- post-up.sh
+++ post-up.sh
@@ -1,3 +1,2 @@
 # This file is executed (not sourced) after loading the main rules.
 # You can use this file to touch files, enable cron-jobs etc.
-exit 0
--- pre-up.sh
+++ pre-up.sh
@@ -1,3 +1,2 @@
 # This file is executed (not sourced) prior to loading the main rules.
 # You can use this file to load modules, adjust /proc settings etc.
-exit 0
