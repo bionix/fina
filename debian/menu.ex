dfb970696fd0484208002f899a4663cc0862a7aa
Alexander Gedak <Alexander@Gedak.net>
2008-02-27 13:22:06 +0000

    Debian package should work
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@57 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fina):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="fina" command="/usr/bin/fina"
--- mkdist.sh
+++ mkdist.sh
@@ -6,11 +6,11 @@ function die() {
 }
 
 
-FVERS=$(./fina.sh --version|head -n1|sed -e 's/.*version //g') || die "Could not determine fina version"
+FVERS=$(./fina --version|head -n1|sed -e 's/.*version //g') || die "Could not determine fina version"
 PNAME="fina-$FVERS"
 
 mkdir "$PNAME" || die "mkdir failed"
-cp -r fina.sh example-rules.d fina.cfg INSTALL COPYING README fina.init fina.init.gentoo post-up.sh pre-up.sh "fina-$FVERS/" || die "cp failed"
+cp -r fina example-rules.d fina.cfg INSTALL COPYING README fina.init fina.init.gentoo post-up.sh pre-up.sh "fina-$FVERS/" || die "cp failed"
 find "$PNAME" -name '.svn' -print0 |xargs -0 rm -r || die "find failed"
 tar czf "$PNAME".tar.gz "$PNAME" || die "tar failed"
 rm -rf "$PNAME" || die "rm failed"
