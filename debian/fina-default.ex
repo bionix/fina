cae7cd8460e588a683d749bae1dfeebba4778a3f
Alexander Gedak <Alexander@Gedak.net>
2008-02-27 09:32:54 +0000

    Debian initial hinzufuegen
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@54 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

---
+++ debian/fina-default.ex
@@ -0,0 +1,10 @@
+# Defaults for fina initscript
+# sourced by /etc/init.d/fina
+# installed at /etc/default/fina by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(fina):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="fina" command="/usr/bin/fina"
