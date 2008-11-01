f4a940da9cbb0081d937319e3c5bcf90407049b9
Tobias Klausmann <klausman@schwarzvogel.de>
2008-11-01 16:00:20 +0000

    New files need packaging
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@71 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- mkdist.sh
+++ mkdist.sh
@@ -10,7 +10,7 @@ FVERS=$(./fina --version|head -n1|sed -e 's/.*version //g') || die "Could not de
 PNAME="fina-$FVERS"
 
 mkdir "$PNAME" || die "mkdir failed"
-cp -r fina example-rules.d fina.cfg INSTALL COPYING README fina.init fina.init.gentoo post-up.sh pre-up.sh "fina-$FVERS/" || die "cp failed"
+cp -r fina fina6 example-rules.d fina.cfg INSTALL COPYING README fina.init fina6.init fina.init.gentoo fina6.init.gentoo post-up.sh pre-up.sh post6-up.sh pre6-up.sh "fina-$FVERS/" || die "cp failed"
 find "$PNAME" -name '.svn' -print0 |xargs -0 rm -r || die "find failed"
 tar czf "$PNAME".tar.gz "$PNAME" || die "tar failed"
 rm -rf "$PNAME" || die "rm failed"
