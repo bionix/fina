af908a78b8020db38457b9839db16c6751839f20
Tobias Klausmann <klausman@schwarzvogel.de>
2008-12-17 14:30:57 +0000

    Logic fix Doc fix
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@76 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- pre6-up.sh
+++ pre6-up.sh
@@ -1,2 +1,4 @@
 # This file is executed (not sourced) prior to loading the main rules.
 # You can use this file to load modules, adjust /proc settings etc.
+#
+# NOTE: This script has to terminate with retval 0 for fina to continue!
