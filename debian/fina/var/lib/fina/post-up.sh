be8e45eba40f706236e66ceb9817b95e69628d4f
Tobias Klausmann <klausman@schwarzvogel.de>
2008-12-17 15:30:25 +0000

    A load of changes, most of them Debian-build related
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@78 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

---
+++ debian/fina/var/lib/fina/post-up.sh
@@ -0,0 +1,4 @@
+# This file is executed (not sourced) after loading the main rules.
+# You can use this file to touch files, enable cron-jobs etc.
+#
+# NOTE: This script has to terminate with retval 0 for fina to continue!
