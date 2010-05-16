84a0d16476bd9cba3f2c43975042f19424a8b2e6
Tobias Klausmann <klausman@schwarzvogel.de>
2010-05-16 10:15:55 +0000

    Make a place to keep dist tarballs Change mkdist do use that dir
    git-svn-id: http://kaini.schwarzvogel.de/svn/repos/fina/trunk@87 bbab99c9-e8d4-40e5-953a-fbd5b2b2805b

--- mkdist.sh
+++ mkdist.sh
@@ -8,9 +8,11 @@ function die() {
 
 FVERS=$(./fina --version|head -n1|sed -e 's/.*version //g') || die "Could not determine fina version"
 PNAME="fina-$FVERS"
+DISTDIR="dist/"
+[[ -d $DISTDIR ]] || mkdir $DISTDIR
 
 mkdir "$PNAME" || die "mkdir failed"
 cp -r fina fina6 example-rules.d fina.cfg INSTALL COPYING README fina.init fina6.init fina.init.gentoo fina6.init.gentoo post-up.sh pre-up.sh post6-up.sh pre6-up.sh "fina-$FVERS/" || die "cp failed"
 find "$PNAME" -name '.svn' -print0 |xargs -0 rm -r || die "find failed"
-tar czf "$PNAME".tar.gz "$PNAME" || die "tar failed"
+tar czf "$DISTDIR/$PNAME".tar.gz "$PNAME" || die "tar failed"
 rm -rf "$PNAME" || die "rm failed"
